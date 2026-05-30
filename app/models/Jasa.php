055b34a5cc3d4fb1ba89da676de03436b3af1669
kiisann <fikisulistiawan@gmail.com>
2026-05-30 13:38:23 +0700

    fix bug

--- app/models/Jasa.php
+++ app/models/Jasa.php
@@ -55,7 +55,7 @@ class Jasa {
 
         mysqli_stmt_bind_param(
             $stmt,
-            "iississ",
+            "iissdss",
             $data['id_user'],
             $data['id_kategori'],
             $data['nama_jasa'],
